3731c9209196c847a973ad63f792ad09ef1f9fda
Gui089 <guilherme@gui>
2024-11-16 21:43:36 -0300

    feat: :white_check_mark:add  About sectin

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script setup>
 import NavBar from './components/NavBar.vue';
 import Header from './components/Header.vue';
+import About from './components/About.vue';
 </script>
 
 <template>
-  <div class="p-10 px-20">
+  <div class="p-10 px-20 flex flex-col gap-5">
 
       <NavBar />
       <Header />
-      About
+      <About />
 
   </div>
 </template>
--- src/components/About.vue
+++ src/components/About.vue
@@ -1,13 +1,119 @@
 <template lang="">
     <div>
-        
+       <h1 class="text-3xl font-bold text-gray-300 mb-5">
+            Sobre mim
+       </h1> 
+
+       
+
+       <div class="flex gap-5">
+         <div class="w-4/6 flex flex-col gap-5">
+            <div class="grid grid-cols-2 gap-5">
+                <div class="rounded-xl p-5 bg-gray-900">
+           <img class="h-72 mx-auto " src="/gui.png" alt="image of a development" />
+           <h1 class="text-xl font-bold text-gray-300 mb-5">
+               Guilherme
+           </h1>
+           <p class="text-gray-500">
+               Sou um Desenvolvedor Full Stack - React, 
+               VueJs, Laravel e Nodejs, 
+               atuei com desenvolvimento de aplicações
+               web com Laravel, VueJs e React, 
+               utlizando PostgreSQL e 
+               MySQL juntamente com Docker e AWS, 
+               preso sempre pelo qualidade do código e 
+               pelo padrão de arquitetura limpa - 
+               Atualmente estou desenvolvendo meu próprio saas, 
+               que está sendo feito em Laravel e VueJS .
+           </p>
+        </div>
+            <div class="rounded-xl p-5 bg-gray-900">
+           <img class="h-72 mx-auto " src="/laravel-vue.png" alt="image of a development" />
+           <h1 class="text-xl font-bold text-gray-300 mb-5">
+               Guilherme
+           </h1>
+           <p class="text-gray-500">
+               Sou um Desenvolvedor Full Stack - React, 
+               VueJs, Laravel e Nodejs, 
+               atuei com desenvolvimento de aplicações
+               web com Laravel, VueJs e React, 
+               utlizando PostgreSQL e 
+               MySQL juntamente com Docker e AWS, 
+               preso sempre pelo qualidade do código e 
+               pelo padrão de arquitetura limpa - 
+               Atualmente estou desenvolvendo meu próprio saas, 
+               que está sendo feito em Laravel e VueJS .
+           </p>
+       </div>
+            </div>
+
+            <div class="rounded-xl p-5 bg-gray-900">
+           <img class="h-72 mx-auto " src="/grid3.png" alt="image of a development" />
+           <h1 class="text-xl font-bold text-gray-300 mb-5">
+               Guilherme
+           </h1>
+           <p class="text-gray-500">
+               Sou um Desenvolvedor Full Stack - React, 
+               VueJs, Laravel e Nodejs, 
+               atuei com desenvolvimento de aplicações
+               web com Laravel, VueJs e React, 
+               utlizando PostgreSQL e 
+               MySQL juntamente com Docker e AWS, 
+               preso sempre pelo qualidade do código e 
+               pelo padrão de arquitetura limpa - 
+               Atualmente estou desenvolvendo meu próprio saas, 
+               que está sendo feito em Laravel e VueJS .
+           </p>
+       </div>
+         </div>
+
+         <div class="w-2/6 flex flex-col gap-5">
+            <div class="rounded-xl p-5 bg-gray-900">
+           <img class="h-72 mx-auto " src="/about-img4.png" alt="image of a development" />
+           <h1 class="text-xl font-bold text-gray-300 mb-5">
+               Guilherme
+           </h1>
+           <p class="text-gray-500">
+               Sou um Desenvolvedor Full Stack - React, 
+               VueJs, Laravel e Nodejs, 
+               atuei com desenvolvimento de aplicações
+               web com Laravel, VueJs e React, 
+               utlizando PostgreSQL e 
+               MySQL juntamente com Docker e AWS, 
+               preso sempre pelo qualidade do código e 
+               pelo padrão de arquitetura limpa - 
+               Atualmente estou desenvolvendo meu próprio saas, 
+               que está sendo feito em Laravel e VueJS .
+           </p>
+       </div>
+            <div class="rounded-xl p-5 bg-gray-900">
+           <img class="h-72 mx-auto " src="/grid4.png" alt="image of a development" />
+           <h1 class="text-xl font-bold text-gray-300 mb-5">
+               Guilherme
+           </h1>
+           <p class="text-gray-500">
+               Sou um Desenvolvedor Full Stack - React, 
+               VueJs, Laravel e Nodejs, 
+               atuei com desenvolvimento de aplicações
+               web com Laravel, VueJs e React, 
+               utlizando PostgreSQL e 
+               MySQL juntamente com Docker e AWS, 
+               preso sempre pelo qualidade do código e 
+               pelo padrão de arquitetura limpa - 
+               Atualmente estou desenvolvendo meu próprio saas, 
+               que está sendo feito em Laravel e VueJS .
+           </p>
+       </div>
+         </div>
+       </div>
+
     </div>
 </template>
 <script>
 export default {
-    
+
 }
 </script>
 <style lang="">
-    
+
 </style>
